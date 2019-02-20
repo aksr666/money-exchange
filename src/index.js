191f8609bfdf0b3ef023adea9c9a7bf910524217
aksr666 <aksr100793@mail.ru>
2019-02-20 16:58:43 +0300

    money exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,42 @@
 // PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+module.exports = makeExchange = sum => {
+  if(sum < 0 || sum == 0) return {};
+  if(sum >= 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+  const obj = {};
+    if((sum - 50) >= 0) {
+      obj['H'] = 0;
+     while(sum >= 50){
+      sum -= 50;
+      obj['H']++;
+     }
+    }
+    if((sum - 25) >= 0) {
+      obj['Q'] = 0;
+     while(sum >= 25){
+      sum -= 25;
+      obj['Q']++;
+     }
+    }
+    if((sum - 10) >= 0) {
+      obj['D'] = 0;
+     while(sum >= 10){
+      sum -= 10;
+      obj['D']++;
+     }
+    }
+    if((sum - 5) >= 0) {
+      obj['N'] = 0;
+     while(sum >= 5){
+      sum -= 5;
+      obj['N']++;
+     }
+    }
+    if((sum - 1) >= 0) {
+      obj['P'] = 0;
+     while(sum > 0){
+      sum -= 1;
+      obj['P']++;
+     }
+    }
+return obj;
 }
